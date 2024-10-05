c0c0a81e07eda68d9510c077086257306a453df0
Khoa hoc DU Lieu 17A1 <nnvinh.dhkl17a1hn@sv.uneti.edu.vn>
2024-10-05 22:03:25 +0700

    hoan thanh bai tap

--- chuong2/bai2.4.py
+++ chuong2/bai2.4.py
@@ -3,8 +3,4 @@ def main():
     doc = xml.dom.minidom.parse("chuong2\\bai2.3.xml")
     print(doc.nodeName)
     print(doc.firstChild.tagName)
-    staff = doc.getElementsByTagName("staff")
-    print("%d staff:" % staff.length)
-    for id in staff:
-        print(id.getAttribute("id"))
 main()
--- chuong2/bai2.5.py
+++ chuong2/bai2.5.py
@@ -1,14 +1,10 @@
-import xml.etree.ElementTree as ET 
 import xml.dom.minidom
-tree = ET.parse('chuong2\\bai2.3.xml')
-doc = xml.dom.minidom.parse("chuong2\\bai2.3.xml") 
-root = tree.getroot()
-name = root.find('name')
-print(name.text)
-nhanvien = root.find("staff")
-print(nhanvien.attrib['id'])
-
-staff = doc.getElementsByTagName("staff")
-print("%d staff:" % staff.length)
-for id in staff:
-    print(id.getAttribute("id"))
+def main():
+    doc = xml.dom.minidom.parse("chuong2\\bai2.3.xml")
+    print(doc.nodeName)
+    print(doc.firstChild.tagName)
+    staff = doc.getElementsByTagName("staff")
+    print("%d staff:" % staff.length)
+    for id in staff:
+        print(id.getAttribute("id"))
+main()
