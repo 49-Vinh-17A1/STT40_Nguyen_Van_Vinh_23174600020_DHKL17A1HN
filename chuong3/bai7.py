3fc493cd132419f4c7db73d12de80eb7e84b249c
Khoa hoc DU Lieu 17A1 <nnvinh.dhkl17a1hn@sv.uneti.edu.vn>
2024-10-19 23:47:00 +0700

    hoan thanh bai tap

--- chuong3/bai7.py
+++ chuong3/bai7.py
@@ -1,19 +1,16 @@
 import numpy as np
 
-# 1. Tạo mảng 2D từ tệp baseball.txt
-# Giả sử tệp có cấu trúc gồm cột chiều cao (inch) và cân nặng (pounds)
 np_baseball = np.loadtxt('chuong3\\baseball_2D.txt', delimiter=',')
 
-# 2. In giá trị của dòng thứ 50 trong np_baseball
+
 print("Dòng thứ 50 trong np_baseball:")
 print(np_baseball[49])
 
-# 3. Cho biết chiều cao của các cầu thủ từ dòng 124 trở đi
 print("\nChiều cao của các cầu thủ từ dòng 124 trở đi:")
-print(np_baseball[123:, 0])  # Giả sử chiều cao nằm ở cột đầu tiên
+print(np_baseball[123:, 0]) 
 
-# 4. Tính chiều cao trung bình của tất cả các cầu thủ
-mean_height = np.mean(np_baseball[:, 0])  # Giả sử chiều cao nằm ở cột đầu tiên
+
+mean_height = np.mean(np_baseball[:, 0])  
 print("\nChiều cao trung bình của các cầu thủ:")
 print(mean_height)
 
