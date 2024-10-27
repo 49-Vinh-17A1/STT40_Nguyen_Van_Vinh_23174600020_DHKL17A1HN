4b89f7a9850a36f546372ee5893ac6cab27ac86b
Khoa hoc DU Lieu 17A1 <nnvinh.dhkl17a1hn@sv.uneti.edu.vn>
2024-10-27 15:55:07 +0700

    hoan thanh bai tap

--- chuong3/bai3.9.py
+++ chuong3/bai3.9.py
@@ -1,35 +1,50 @@
+import pandas as pd
 import numpy as np
-data = np.genfromtxt('chuong3\\euro2012.csv', delimiter=',', dtype=str, skip_header=1)
-teams = data[:, 0]  
-yellow_cards = data[:, 1].astype(int) 
-red_cards = data[:, 2].astype(int)
 
+data = {
+    'Team': ['England', 'France', 'Germany', 'Italy', 'Russia', 'Spain', 'Portugal', 'Greece'],
+    'Goals': [5, 3, 6, 4, 5, 12, 6, 2],
+    'Shooting Accuracy': [45.0, 38.0, 44.0, 41.0, 50.0, 55.0, 42.0, 35.0],
+    'Yellow Cards': [7, 6, 9, 10, 5, 11, 8, 4],
+    'Red Cards': [0, 1, 0, 1, 0, 0, 0, 0]
+}
 
-print("Danh sách các đội tham gia Euro 2012:")
-print(teams)
+df = pd.DataFrame(data)
 
-print("\nSố thẻ vàng của mỗi đội:")
-for team, yellow in zip(teams, yellow_cards):
-    print(f"{team}: {yellow}")
-
-print("\nSố thẻ đỏ của mỗi đội:")
-for team, red in zip(teams, red_cards):
-    print(f"{team}: {red}")
-
-
-sorted_indices_yellow = np.argsort(yellow_cards)  
-sorted_teams_yellow = teams[sorted_indices_yellow] 
-sorted_yellow_cards = yellow_cards[sorted_indices_yellow] 
-
-print("\nDanh sách các đội sắp xếp theo số thẻ vàng (tăng dần):")
-for team, yellow in zip(sorted_teams_yellow, sorted_yellow_cards):
-    print(f"{team}: {yellow}")
-
-
-sorted_indices_red = np.argsort(red_cards)
-sorted_teams_red = teams[sorted_indices_red]  
-sorted_red_cards = red_cards[sorted_indices_red] 
-
-print("\nDanh sách các đội sắp xếp theo số thẻ đỏ (tăng dần):")
-for team, red in zip(sorted_teams_red, sorted_red_cards):
-    print(f"{team}: {red}")
+#df.to_csv('chuong3/euro2012.csv', index=False)
+euro = pd.read_csv('chuong3\euro2012.csv')
+print("giá trị cột goal là: ")
+#1
+print(euro['Goals']) 
+#2
+nums_team = euro['Team'].nunique()
+print(nums_team)
+#3
+print("thông tin euro2012 là:\n ", euro)
+#4
+discipline = euro.drop(['Goals','Shooting Accuracy'],axis = 1)
+discipline = pd.DataFrame(discipline)
+print(discipline)
+#5
+dis_sort_yellow = discipline.sort_values(by='Yellow Cards')[::-1]
+print(dis_sort_yellow)
+dis_sort_red = discipline.sort_values(by='Red Cards')[::-1]
+print(dis_sort_red)
+#6
+yellows = dis_sort_red['Yellow Cards']
+print("trung bình Yellows Cảd la: ",np.mean(yellows))
+goals = euro[euro['Goals'] >6]
+print("đội ghi hơn 6 bàn thắng là: ",goals)
+#7
+start_G= euro[euro['Team'].str.startswith('G')]
+print("Đội bắt đầu bằng chứ G là: \n",start_G)
+#8
+print("7 dòng đồng của euro12 là: \n",euro.head(7))
+#9
+print("tất cả các cột trừ 3 cột cuối là: \n",euro.iloc[:,:-3])
+#10
+print("tất cả các cột là: ",euro)
+#11
+team_shoot =euro[['Team','Shooting Accuracy']]
+team_shoot = team_shoot[team_shoot['Team'].isin(['England','Italy','Russia'])]
+print("câu 11: \n",team_shoot)
